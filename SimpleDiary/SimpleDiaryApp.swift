d87e095778be5f0b0fc79fbffdfbb4408c3ed5ca
sglee237 <sglee237@gmail.com>
2023-01-26 15:54:07 +0900

    Initial Commit

---
+++ SimpleDiary/SimpleDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDiaryApp.swift
+//  SimpleDiary
+//
+//  Created by sglee237 on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

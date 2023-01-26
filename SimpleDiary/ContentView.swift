2cec2dc79c0da5be288d8edf00901989f1d91a52
sglee237 <sglee237@gmail.com>
2023-01-26 17:35:47 +0900

    상단 버튼 만들기

--- SimpleDiary/ContentView.swift
+++ SimpleDiary/ContentView.swift
@@ -7,16 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    
+struct TopArea: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            Button {
+                
+            } label: {
+                Text("한줄 일기 기록")
+                    .font(.system(size: 33))
+                    .frame(maxWidth: .infinity)
+            }
+            .padding()
         }
+        .background(.white)
+        .cornerRadius(20)
         .padding()
+    }
+}
+
+
+
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.gray.ignoresSafeArea()
+                .opacity(0.2)
+            
+            VStack {
+                // Area:1 - TopButton
+                TopArea()
+                // Area:2 - ScrollArea
+            }
+            .padding()
+        }
     }
 }
 
